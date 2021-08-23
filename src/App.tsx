e178bd791e66fda5e1630756bf7c8851546f54fa
azharimm <azhari.marzan@gmail.com>
2021-08-24 06:16:30 +0700

    chore: fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+//Components
+import { LinearProgress, Grid, Drawer, Badge } from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
+//Styles
+import { Wrapper } from "./App.styles";
+
+export type CartItemType = {
+	id: number;
+	category: string;
+	description: string;
+	image: string;
+	price: number;
+	title: string;
+	amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> =>
+	await (await fetch("https://fakestoreapi.com/products")).json();
 
 const App = () => {
-	return (
-		<div className="App">
-			Start
-		</div>
+	const { data, isLoading, error } = useQuery<CartItemType[]>(
+		"products",
+		getProducts
 	);
-}
+	const getTotalItems = () => null;
+	const handleAddToCart = () => null;
+	const removeFromCart = () => null;
+
+	if(isLoading) return <LinearProgress />
+	if(error) return <div>Somethign went wrong!</div>
+
+	return <div className="App">Start</div>;
+};
 
 export default App;
